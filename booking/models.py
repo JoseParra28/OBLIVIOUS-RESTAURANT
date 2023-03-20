96f6ded30abf54929995772c40735dbf83370343
JoseParra28 <iliftandcook@gmail.com>
2023-03-20 11:19:09 +0000

    create table model

--- booking/models.py
+++ booking/models.py
@@ -3,4 +3,16 @@ from django.conf import settings
 
 
 
-
+class Table(models.Model):
+    TABLE_CATEGORIES = (
+        ('TWO', 'TWO'),
+        ('THREE', 'THREE'),
+        ('FOUR', 'FOUR'),
+        ('FIVE', 'FIVE'),
+        ('GROUP', 'GROUP')
+    )
+    table_number = models.IntegerField() 
+    categotie = models.CharField(max_length=20, choices=TABLE_CATEGORIES)
+    guests = models.IntegerField()
+    def __str__(self):
+        return f'table {self.table_number}. For {self.guests} people'
