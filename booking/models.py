1651cf7ad7dfb39fa8a945279279963130e98f1a
JoseParra28 <iliftandcook@gmail.com>
2023-03-20 11:33:46 +0000

    create booking model

--- booking/models.py
+++ booking/models.py
@@ -16,3 +16,14 @@ class Table(models.Model):
     guests = models.IntegerField()
     def __str__(self):
         return f'table {self.table_number}. For {self.guests} people'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings. AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False,)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    arrival_time = models.DateTimeField()
+    departure_time = models.TimeField()
+
+    def __str__(self):
+        return f'{self.user} has booked {self.table} on {self.arrival_time}'
+
