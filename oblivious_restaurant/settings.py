6279b1da1454f2f92d2111c9c40c6508a8b67652
JoseParra28 <iliftandcook@gmail.com>
2023-04-09 18:46:46 +0000

    save

--- oblivious_restaurant/settings.py
+++ oblivious_restaurant/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary'
+     'cloudinary_storage',
     'booking',
 ]
 
